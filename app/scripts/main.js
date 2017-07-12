c5d0af311b15a42923afb6e5fcdae0fa8c379b9d
Corey Crawford <corey.n.crawford@gmail.com>
2017-07-12 00:27:19 -0400

    Fourth Commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
 console.log('\'Allo \'Allo!');
+
+import Vue from 'vue'
+
+new Vue({
+    el: '#list',
+    data: {
+        list: "<ul><li>HTML5 Boilerplate</li><li>Sass</li><li>Modernizr</li><li>jQuery</li><li>VueJS</li></ul>"
+    }
+});
+
+var example1 = new Vue({
+  el: '#example-1',
+  data: {
+    items: [
+      { message: 'HTML5 Boilerplate' },
+      { message: 'Sass' },
+      { message: 'Modernizr' },
+      { message: 'jQuery' },
+      { message: 'VueJS' }
+    ]
+  }
+});
